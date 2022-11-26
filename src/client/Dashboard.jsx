85875986c28572fe00c71432acb38512e80121c1
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-11-26 10:58:05 +0300

    Deconnexion client

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <div>
         <div className="Dashboard section-title"  data-aos="fade-up">
-          <h2>Resume</h2>
+          <h2>Bienvenue cher Client</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
     </div>
--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,7 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { loginService } from '../../_services/login.service'
 
 export default function Header() {
+
+    let navigate = useNavigate()
+
+    const logout = () => {
+        loginService.logout()
+        navigate('/')
+    }
+
   return (
         <header id="header">
             <div className="d-flex flex-column">
@@ -16,7 +26,8 @@ export default function Header() {
                         <li><Link to="/client/dashboard" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Accueil</span></Link></li>
                         <li><Link to="/client/moncompte" className="nav-link scrollto"><i className="bx bx-user"></i> <span>Mon Compte</span></Link></li>
                         <li><Link to="/client/historiqueintervention" className="nav-link scrollto"><i className="bx bx-calendar-event"></i> <span>Historique Intervention</span></Link></li>
-                        <li><Link to="/client/factureintervention" className="nav-link scrollto"><i className="bx bx-user"></i> <span>Facture Intervention</span></Link></li>
+                        <li><Link to="/client/factureintervention" className="nav-link scrollto"><i className="bx bx-bookmark-minus"></i> <span>Facture Intervention</span></Link></li>
+                        <li><a href="#" onClick={logout} className="nav-link scrollto"><i className="bx bx-lock-open-alt"></i> <span>Se déconnecter</span></a></li>
                     </ul>
                 </nav>
             </div>
